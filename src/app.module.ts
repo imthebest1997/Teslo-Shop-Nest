f864048958932696863e645a17b3ced3aa3e156b
Alejandro G. <imthebest_1997@hotmail.com>
2024-05-25 15:50:58 -0500

    Database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, // Don't use in production
+    }),
+  ],
 })
 
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function main() {
+
   const app = await NestFactory.create(AppModule);
+
+  // Listen on port 3000
   await app.listen(3000);
+
+  // Global prefix
+  app.setGlobalPrefix('api');
 }
 
 main();
