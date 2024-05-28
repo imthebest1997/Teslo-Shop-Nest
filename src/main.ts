6660b55bbddf4a89f7f0b46b281c153f872d96e4
Alejandro G. <imthebest_1997@hotmail.com>
2024-05-28 16:05:12 -0500

    CRUD Products entity

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function main() {
 
   const app = await NestFactory.create(AppModule);
 
+  // Global prefix
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      // transform: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+
   // Listen on port 3000
   await app.listen(3000);
 
-  // Global prefix
-  app.setGlobalPrefix('api');
 }
 
 main();
