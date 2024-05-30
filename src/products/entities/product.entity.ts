1e3c4689a013e7ab1f4ff64e38e9b274f1ef30dd
Alejandro G. <imthebest_1997@hotmail.com>
2024-05-30 14:37:16 -0500

    Rename tables

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,7 +1,7 @@
 import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 import { ProductImage } from "./product-image.entity";
 
-@Entity()
+@Entity({ name: 'products' })
 export class Product {
 
     @PrimaryGeneratedColumn('uuid')
@@ -49,8 +49,7 @@ export class Product {
         default: [],
     })
     tags: string[];
-
-
+    
     @OneToMany(
         () => ProductImage,
         (productImage) => productImage.product,
